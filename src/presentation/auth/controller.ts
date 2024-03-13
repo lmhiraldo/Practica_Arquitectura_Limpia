b414befcc78d5c955b76c200593d454002c19681
lmhiraldo <laura.martinez.dev@gmail.com>
2024-03-13 07:39:34 +0100

    Rutas y Controladores-primer commit

---
+++ src/presentation/auth/controller.ts
@@ -0,0 +1,18 @@
+import { Request,Response } from "express";
+
+
+export class AuthController {
+  //DI
+
+  constructor() {}
+
+  registerUser = (req: Request, res: Response) => {
+
+    res.json('registerUser controller')
+  };
+
+  loginUser = (req: Request, res: Response) => {
+
+    res.json("loginUser controller");
+  };
+}
---
+++ src/presentation/auth/routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { AuthController } from "./controller";
+
+export class AuthRoutes {
+  static get routes(): Router {
+    const router = Router();
+    const controller = new AuthController();
+    //Definir todas mis rutas principales
+    router.post("/login", controller.loginUser);
+    
+    router.post("/register", controller.registerUser);
+    return router;
+  }
+}
